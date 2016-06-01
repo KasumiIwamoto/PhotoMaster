a3de84001e21edf5d9790e509f19e6c067909091
岩本果純 <kpe.359@icloud.com>
2016-06-01 11:50:02 +0900

    second  commit

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -25,11 +25,17 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
             self.presentViewController(picker,animated:true,completion:nil)
         }
     }
-    func imagePickerController(picker:UIImagePickerController!,didFinishPickerImage image:UIImage!,editingInfo:NSDictionary!){
-        self.dismissViewControllerAnimated(true,completion:nil)
+    //func imagePickerController(picker:UIImagePickerController!,didFinishPickerImage image:UIImage!,editingInfo:NSDictionary!){
+       // self.dismissViewControllerAnimated(true,completion:nil)
         
         //画像出力
+       // photoImageView.image = image
+       // print(image)
+    //}
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+         self.dismissViewControllerAnimated(true,completion:nil)
         photoImageView.image = image
+        print(image)
     }
     @IBAction func selectButtonTapped(sender:UIButton){
         let alertController = UIAlertController(title:"画像の取得先を選択",message:nil,preferredStyle:.ActionSheet)
@@ -47,7 +53,6 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
         alertController.addAction(firstAction)
         alertController.addAction(secondAction)
         alertController.addAction(cancelAction)
-        
         //アラート表示
         presentViewController(alertController,animated:true,completion:nil)
     }
